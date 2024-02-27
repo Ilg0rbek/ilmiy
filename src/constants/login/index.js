ba8f439a0cf48dc0991124c75887fe0855e7fc42
samandar <abduraxmonov200211@gmail.com>
2024-02-27 15:43:28 +0500

    add loading for login component

--- src/constants/login/index.js
+++ src/constants/login/index.js
@@ -6,28 +6,32 @@ import { useNavigate, Link } from "react-router-dom";
 import { login } from "../../redux/reducers/auth.store";
 import { useEffect } from "react";
 import axiosConfig, { setAuthToken } from "../../redux/baseUrl";
+import { Oval } from 'react-loader-spinner'
+
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [data, setData] = useState()
-  
-  const token =  sessionStorage.getItem("token")
- 
+  const [loading, setLoading] = useState(false)
+  const token = sessionStorage.getItem("token")
+
   const onFinish = (values) => {
+    setLoading(true)
     axiosConfig.post("/auth/login", values).then(res => {
-      console.log(res.data);
+      console.log(res.data.data);
       setData(res.data.data)
       sessionStorage.setItem("user", res.data.data.role)
       sessionStorage.setItem("userId", res.data.data._id)
       sessionStorage.setItem("token", res.data.token)
+      setLoading(false)
     }).catch(err => {
       console.log(err);
     })
 
   };
 
- 
+
 
   useEffect(() => {
     if (data?.role == "admin") {
@@ -44,48 +48,63 @@ const Login = () => {
   };
   console.log("state", data);
   return (
-    <div className="login-page">
-      <div className="login-box">
-        <div className="illustration-wrapper">
-          <img
-            src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700"
-            alt="Login"
-          />
+    loading ? <div className="for_loader2">
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel='oval-loading'
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+
+      />
+    </div> :
+      <div className="login-page">
+        <div className="login-box">
+          <div className="illustration-wrapper">
+            <img
+              src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700"
+              alt="Login"
+            />
+          </div>
+          <Form
+            name="login-form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}>
+            <p className="form-title">Welcome to</p>
+            <p>Login to the Dashboard</p>
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}>
+              <Input placeholder="Username" />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}>
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button">
+                LOGIN
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
-        <Form
-          name="login-form"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}>
-          <p className="form-title">Welcome to</p>
-          <p>Login to the Dashboard</p>
-          <Form.Item
-            name="username"
-            rules={[
-              { required: true, message: "Please input your username!" },
-            ]}>
-            <Input placeholder="Username" />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            rules={[
-              { required: true, message: "Please input your password!" },
-            ]}>
-            <Input.Password placeholder="Password" />
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button">
-              LOGIN
-            </Button>
-          </Form.Item>
-        </Form>
       </div>
-    </div>
   );
 };
 
