27d0288d059a5c2b724aedf92f3cc1b3019c2c07
AkbarshohMamadaliyev <akbarshokhmamadaliev@gmail.com>
2024-10-11 17:39:25 +0500

    11.10.2024

--- src/components/pages/mdiUser/themes/Themes.jsx
+++ src/components/pages/mdiUser/themes/Themes.jsx
@@ -1,7 +1,63 @@
-function Themes() {
-  console.log("Themes");
+import styles from "./themes.module.css";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-  return <div>Themes</div>;
+function Themes() {
+  const { academic_year_id, kafedra_id } = useParams();
+  const [theme, setTheme] = useState([]);
+
+  useEffect(() => {
+    const getThemes = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://md-themes-api.adu.uz/api/topics?kafedra_id=${kafedra_id}`
+        );
+
+        setTheme(data.topics);
+        console.log("response", data.topics);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getThemes();
+  }, []);
+
+  return (
+    <div className={styles.theme}>
+      <h3 className={styles.themeTitle}>MD Mavzulari</h3>
+      <div className={styles.themeWrapper}>
+        <table className={styles.themeTable}>
+          <thead>
+            <tr>
+              <th scope="col">Professor o'qituvchi</th>
+              <th scope="col">Ilmiy daraja va unvon</th>
+              <th scope="col">Mavzu</th>
+              <th scope="col">Band qilish</th>
+            </tr>
+          </thead>
+          <tbody>
+            {theme.map((item) => (
+              <tr key={item.id}>
+                <td data-label="Professor o'qituvchi">
+                  {item.professor.fullname}
+                </td>
+                <td data-label="Ilmiy daraja va unvon">
+                  {item.professor.position}
+                </td>
+                <td data-label="Mavzu">{item.name}</td>
+                {item.active === 1 ? (
+                  <td>Band qilish</td>
+                ) : (
+                  <td>Student full name</td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Themes;
